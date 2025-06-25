3a0459cdf480f0336ce165e3d7dca69c31e8a5eb
riandias1331 <pereirarian@gmail.com>
2025-06-24 23:24:18 -0300

    initial commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 const connectDB = (app) => {
-    mongoose.connect(process.env.DATABASE_URL)
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
         .then(() => {
             console.log('Connected to database')
             app.emit('Database')
